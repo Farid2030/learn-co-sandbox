730810d92c9eb849df1732e6955e1efd84e94a98
Farid Mansour Thabet Alabsi <alabsifarid@gmail.com>
2018-06-26 11:59:31 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function say(firstName) {
+  return `Hello, ${firstName}!`
+}
+console.log(say('farid'))
+ /*
+sayHelloTo('Isabel') // "Hello, Isabel!"
+sayHelloTo("Jane") // "Hello, Jane!"
+sayHelloTo("R2-D2") // "Hello, R2-D2!"
+sayHelloTo(1) // "Hello, 1!"
+ 
+function say(greeting, firstName) {
+  console.log(`${greeting}, ${firstName}!`)
+}
+say("Julio", "hello")
+function add(x, y) {
+  return x + y
+}
+console.log(add(1, 9000))
+function say(greeting, firstName) {
+  console.log('I was called!')
+  return `${greeting}, ${firstName}!`
+}
+console.log(say("Howdy", "partner"))*/
